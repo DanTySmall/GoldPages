60f9ce1f94e4e575f0254dcb53ea480f2a135f52
Wlphillips <Wlphillips.home@gmail.com>
2024-05-17 16:32:17 -0400

    Update GoldSearchContacts.php
    Fixed database password

--- LAMPAPI/GoldSearchContacts.php
+++ LAMPAPI/GoldSearchContacts.php
@@ -5,7 +5,7 @@
 	$search = "%" . $inData["search"] . "%";
 	$userId = $inData["userId"];
 
-	$conn = new mysqli("localhost", "TheBeast", "Root1Password", "COP4331");
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
